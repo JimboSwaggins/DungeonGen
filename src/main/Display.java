f9803e8d65634ef88d67c9134dd1af516f57780d
Sloop Listman <NahteKavlozs@gmail.com>
2018-03-20 11:50:49 -0700

    0.0.1a

--- src/main/Display.java
+++ src/main/Display.java
@@ -1,11 +1,24 @@
 package main;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
 
 public class Display {
 	public void createAndShowGUI() {
 		JFrame mainWindow = new JFrame();
+		mainWindow.setLayout(new GridBagLayout());
+		GridBagConstraints g = new GridBagConstraints();
+		
+		JTextArea display = new JTextArea();
+		
+		g.gridx = 0;
+		g.gridy = 0;
+		
+		mainWindow.add(display);
 		
 		
 		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
