b2238974f04aa462678cc9adee50d4a27e193b25
Sloop Listman <NahteKavlozs@gmail.com>
2018-03-20 11:45:07 -0700

    0.0.1
    - Added Display

---
+++ src/main/Display.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Display {
+	public void createAndShowGUI() {
+		JFrame mainWindow = new JFrame();
+		
+		
+		mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
+	}
+}
